38ee8453283ea7fa0921aa716439d0538a295e0f
Laura Steiner <steiner.laura@gmail.com>
2025-04-09 15:03:43 -0700

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Laura Steiner on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
