52a5e74d2f7f82bfb54d422e04b9920d7b3103dc
Laura Steiner <steiner.laura@gmail.com>
2025-04-10 16:15:24 -0700

    Add rectangle as a divider, using geometry reader to make its width relative to the device.

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -12,43 +12,51 @@ struct ContentView: View {
 	@State private var offset = 0.0
 	
 	var body: some View {
-		VStack {
-			Spacer()
-			
-			Text(messageString)
-				.multilineTextAlignment(.center)
-				.minimumScaleFactor(0.5)
-				.font(.largeTitle)
-				.fontWeight(.heavy)
-				.foregroundStyle(Color.red)
-				.padding()
-				.frame( height: 150)
-				.frame( maxWidth: .infinity)
-				//.border(.orange, width: 2)
-				.cornerRadius(20)
-			
-			Spacer()
-			
-			HStack{
-				Button("Awesome"){
-					messageString = "You Are Awesome!"
-					offset -= 10.0
-				}
-				
+		GeometryReader { geometry in
+			VStack {
 				Spacer()
 				
-				Button("Great"){
-					messageString = "You Are Great!"
-					offset += 10.0
+				Text(messageString)
+					.multilineTextAlignment(.center)
+					.minimumScaleFactor(0.5)
+					.font(.largeTitle)
+					.fontWeight(.heavy)
+					.foregroundStyle(Color.red)
+					.padding()
+					.frame( height: 150)
+					.frame( maxWidth: .infinity)
+					.border(.orange, width: 2)
+					.cornerRadius(20)
+				
+				Spacer()
+					.border(.blue, width: 2)
+				
+				//Divider()
+				
+				Rectangle()
+					.fill(.indigo)
+					.frame(width: geometry.size.width * 2/3, height: 1)
+				HStack{
+					Button("Awesome"){
+						messageString = "You Are Awesome!"
+						offset -= 10.0
+					}
+					
+					Spacer()
+					
+					Button("Great"){
+						messageString = "You Are Great!"
+						offset += 10.0
+					}
 				}
+				.buttonStyle(.borderedProminent)
+				.padding()
+				.border(.purple, width: 5)
 			}
-			.buttonStyle(.borderedProminent)
-			.padding()
-			//.border(.purple, width: 5)
+			//.border(Color.blue)
+			//.padding()
+			//.background(Color.yellow.opacity(0.4))
 		}
-		//.border(Color.blue)
-		//.padding()
-		//.background(Color.yellow.opacity(0.4))
 	}
 }
 
